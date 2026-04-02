a2c5ca9ff13d80b4bf05acc363a933ff0c5242ed
achint <achintj96244@gmail.com>
2026-04-02 12:56:03 +0530

    feat: remove role from user validator and add updateCustomerSchema to customer validator

--- backend/src/validators/customer.validator.ts
+++ backend/src/validators/customer.validator.ts
@@ -16,3 +16,20 @@ export const createCustomerSchema = z.object({
     premiumAmount: z.string().optional(),
     priceDifference: z.string().optional(),
 });
+
+export const updateCustomerSchema = z.object({
+    name : z.string().min(1,"Name is required").optional(),
+    mobile: z.string().min(1,"Mobile is Required").regex(/^\d{10}$/,"Mobile must be exact 10 digits").optional(),
+    email: z.string().email("Invalid email format").optional(),
+    address: z.string().optional(),
+    bikeModel: z.string().optional(),
+    purchaseDate: z.string().optional(),
+    modeOfPayment: z.string().optional(),
+    cash: z.string().optional(),
+    credit: z.string().optional(),
+    creditor: z.boolean().optional(),
+    promiseDate: z.string().nullable().optional(),
+    priceByCustomer: z.number().optional(),
+    premiumAmount: z.string().optional(),
+    priceDifference: z.string().optional(),
+}).strict();
