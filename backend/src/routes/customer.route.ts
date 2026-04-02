af3fc47b7a5d04ee62b58cb2149d9cdc06c91bf1
achint <achintj96244@gmail.com>
2026-04-02 13:17:21 +0530

    feat: secure customer routes with authentication and admin authorization middleware

--- backend/src/routes/customer.route.ts
+++ backend/src/routes/customer.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import * as customerController from "../controllers/customer.controller";
+import {adminMiddleware,authMiddleware} from "../middlewares/authMiddleware"
 
 const router = express.Router();
 
-router.post("/", customerController.createCustomer);
-router.get("/", customerController.getCustomers);
-router.get("/:id", customerController.getCustomerById);
-router.put("/:id", customerController.updateCustomer);
-router.delete("/:id", customerController.deleteCustomer);
+router.post("/",authMiddleware, customerController.createCustomer);
+router.get("/",authMiddleware, customerController.getCustomers);
+router.get("/:id",authMiddleware, customerController.getCustomerById);
+router.put("/:id",authMiddleware, customerController.updateCustomer);
+router.delete("/:id",authMiddleware,adminMiddleware,  customerController.deleteCustomer);
 export default router;
