d28936e278ac43801d9eeb2d30666513830a3d35
achint <achintj96244@gmail.com>
2026-04-02 13:17:14 +0530

    feat: restrict sensitive user and customer routes with auth and admin middleware

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,16 +1,16 @@
 import express from "express";
 import * as authController from "../controllers/auth.controller";
-import { authMiddleware } from "../middlewares/authMiddleware";
+import { authMiddleware,adminMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 // Public routes
-router.post("/register", authController.register);
+router.post("/register",authMiddleware, adminMiddleware, authController.register);
 router.post("/login", authController.login);
 
 // Protected routes
 router.get("/me", authMiddleware, authController.getUser);
 router.put("/me", authMiddleware, authController.updateUser);
-router.delete("/me", authMiddleware, authController.deleteUser);
+router.delete("/me", authMiddleware, adminMiddleware, authController.deleteUser);
 
 export default router;
