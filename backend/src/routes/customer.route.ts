c74d377c6f743b21445dcc99b78a8305dfe49ce0
achint <achintj96244@gmail.com>
2026-04-02 12:55:56 +0530

    feat: add updateCustomerSchema, remove role from updateSchema, and update customer route paths

--- backend/src/routes/customer.route.ts
+++ backend/src/routes/customer.route.ts
@@ -3,8 +3,9 @@ import * as customerController from "../controllers/customer.controller";
 
 const router = express.Router();
 
-router.post("/create", customerController.createCustomer);
-router.get("/list", customerController.getCustomers);router.get("/:id", customerController.getCustomerById);
+router.post("/", customerController.createCustomer);
+router.get("/", customerController.getCustomers);
+router.get("/:id", customerController.getCustomerById);
 router.put("/:id", customerController.updateCustomer);
 router.delete("/:id", customerController.deleteCustomer);
 export default router;
